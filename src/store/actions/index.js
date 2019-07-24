33bcb0915f2532e90e2b31c98664c31609636c16
onetrckchris <onetrckchris@gmail.com>
2019-07-24 16:14:05 -0700

    completed mvp. Now able to add features to your car after which those features will be removed from the features list and added to the car features. In the proccess of styling

---
+++ src/store/actions/index.js
@@ -0,0 +1,10 @@
+export const REMOVE_FEATURE = 'REMOVE_FEATURE';
+export const ADD_FEATURE = 'ADD_FEATURE';
+
+export const removeFeature = (item) => {
+    return { type: REMOVE_FEATURE, payload: item };
+}
+
+export const addFeature = (item) => {
+    return { type: ADD_FEATURE, payload: item };
+}
